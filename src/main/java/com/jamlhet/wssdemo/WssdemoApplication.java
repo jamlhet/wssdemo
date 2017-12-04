d822c5ec462f2c471fa5abec262e96e37d38a30a
jamlhet <jamlhet@gmail.com>
2017-12-03 19:59:25 -0500

    first commit

---
+++ src/main/java/com/jamlhet/wssdemo/WssdemoApplication.java
@@ -0,0 +1,12 @@
+package com.jamlhet.wssdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WssdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WssdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jamlhet/wssdemo/WssdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jamlhet.wssdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WssdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
